01af6547040c4ceb048be7803ad392e6595da4cc
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2020-02-25 20:17:21 -0800

    Basic species profile model
    Model has common name, species, max size, and water type fields in model.

--- aquanet/speciesprofile/models.py
+++ aquanet/speciesprofile/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    common_name = models.CharField(max_length=200)
+    species = models.CharField(max_length=200)
+    max_size = models.FloatField()
+    water_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        if self.common_name:
+            return self.common_name
+        else:
+            return self.species
