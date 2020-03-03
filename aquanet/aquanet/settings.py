e700bd5a53ed0d332c52de4ab9cac57c29de690b
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2020-03-02 23:46:14 -0800

    Start Users App

--- aquanet/aquanet/settings.py
+++ aquanet/aquanet/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'speciesprofile.apps.SpeciesprofileConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
