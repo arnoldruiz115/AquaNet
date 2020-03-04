3c8b91ac6f2bc568958d7f415927bd57714a404f
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2020-03-03 20:28:26 -0800

    Basic user registration, login, logout, and user profile

--- aquanet/users/views.py
+++ aquanet/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created! You can now login.')
+            return redirect('users:login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def user_profile(request):
+    return render(request, 'users/userprofile.html')
