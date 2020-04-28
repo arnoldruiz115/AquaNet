0f37c191e9d24fd47b2cfb5d6b03133f71cf42e7
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2020-04-27 19:32:15 -0700

    Make image re-ordering responsive

--- aquanet/speciesprofile/static/speciesprofile/js/sortableImages.js
+++ aquanet/speciesprofile/static/speciesprofile/js/sortableImages.js
@@ -35,18 +35,30 @@ $( function() {
     });
 
     $( "#sortable" ).on( "sortupdate", function( event, ui ) {
-            $("#sortable").sortable("refresh");
-            var i = 0;
-            $('#sortable > li').each(function(){
-                $(this).attr('data-slide-to', i)
-                i++;
-                slideNum = $(this).attr('data-slide-to');
-                imageId = $(this).children().attr('img-id');
-                image_url = $(this).children().attr('src');
-                $("#image"+slideNum).attr("src", image_url);
-                $("#delete"+slideNum).val(imageId);
-            });
-        } );
+        var i = 0;
+        $('#sortable > li').each(function(){
+            $(this).attr('data-slide-to', i)
+            i++;
+            slideNum = $(this).attr('data-slide-to');
+            imageId = $(this).children().attr('img-id');
+            image_url = $(this).children().attr('src');
+            $("#image"+slideNum).attr("src", image_url);
+            $("#delete"+slideNum).val(imageId);
+        });
+
+        //After the sort update make the recently clicked the active image
+        // clear active thumbnail and make the most recently moved thumbnail active
+        $(this).children().removeClass('active');
+        last_moved = ui.item;
+        last_moved.addClass('active');
+
+        // clear active image and make the slide corresponding to the recently moved thumbnail active
+        $('.carousel-inner').children().removeClass('active');
+        slideTo = last_moved.attr('data-slide-to');
+        $("#inner-img" + slideTo).addClass('active');
+
+    });
+
     $("#saveImages").click(function(){
         var new_order = $( "#sortable" ).sortable('toArray', {attribute: "order-id"});
         $("#saveImages").val(new_order);
